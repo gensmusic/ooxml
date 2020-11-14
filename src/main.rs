9f34704f6605fee71aaf5dea9581a481836b8f1e
gensmusic <gensmusic@163.com>
2020-11-15 00:20:03 +0800

    more parsing

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,7 @@ fn main() -> Result<()> {
         .context("found no word/document.xml")?;
 
     // xml parse
+    let mut doc_parsing = Parsing::new();
     let parser = EventReader::new(word_doc);
     let mut depth = 0;
     for e in parser {
@@ -65,47 +66,33 @@ fn main() -> Result<()> {
                 if opt.verbose {
                     print_xml_owned_name(&name, depth, true);
                 }
-                if name.local_name.ne("r") {
-                    let attrs: Vec<_> = attributes
-                        .iter()
-                        .map(|a| format!("{}={}", a.name.local_name, a.value))
-                        .collect();
-                    println!(
-                        "{}{}, attributes: {:?}",
-                        " ".repeat(depth),
-                        name.local_name,
-                        attrs
-                    );
-                }
                 depth += 1;
+                doc_parsing.feed_element(name, attributes);
             }
             XmlEvent::EndElement { name } => {
                 depth -= 1;
                 if opt.verbose {
                     print_xml_owned_name(&name, depth, false);
                 }
+                doc_parsing.fish_feed_element();
             }
-            XmlEvent::Comment(ref data) => {
-                println!(r#"Comment("{}")"#, data.escape_debug());
-            }
-            XmlEvent::CData(ref data) => {
-                println!(r#"CData("{}")"#, data.escape_debug());
-            }
-            XmlEvent::Characters(ref data) => {
+            XmlEvent::Comment(ref data) => {}
+            XmlEvent::CData(ref data) => {}
+            XmlEvent::Characters(data) => {
                 println!(
                     r#"{}Characters("{}")"#,
                     " ".repeat(depth),
                     data.escape_debug()
                 );
+                doc_parsing.feed_characters(data);
             }
-            XmlEvent::Whitespace(ref data) => {
-                println!(r#"Whitespace("{}")"#, data.escape_debug());
-            }
+            XmlEvent::Whitespace(ref data) => {}
             _ => {
                 // TODO
             }
         }
     }
+    print_elements(&doc_parsing.root);
     Ok(())
 }
 
@@ -122,59 +109,60 @@ fn print_xml_owned_name(name: &OwnedName, indent: usize, start: bool) {
     println!("{}", name.local_name);
 }
 
-struct Paragraph {
-    property: Option<ParagraphProperty>,
-    runs: Vec<Run>,
-}
-
-struct ParagraphProperty {
-    // paragraph 的默认 run property
-    run_property: Option<RunContentColor>,
-    // TODO 省略其他..
-}
-
-struct Run {
-    property: RunProperty,
-    contents: Vec<RunContent>,
-}
-
-struct RunProperty {
-    bold: bool,
-    italic: bool,
-    color: Option<RunContentColor>,
-    // TODO 省略其他..
-}
-
-/// Run content color property.
-struct RunContentColor {
-    val: Option<String>,
-    theme_color: Option<String>,
-    // TODO 省略其他..
-}
-
+#[derive(Debug)]
 enum ElementType {
-    Document,
-    Body,
-    Paragraph,
-    Run,
-    Text,
+    Document(String),
+    Body(String),
+    Paragraph(String),
+    Run(String),
+    Text(String),
     /// 属性
-    Property,
+    ParagraphProperty(String),
+    RunProperty(String),
+    Color(String),
 
     /// 其他剩余的不支持的类型
-    Unknown,
+    Unknown(String),
 }
 impl ElementType {
-    fn from_str(s: &str) -> Self {
-        match s {
-            "document" => Self::Document,
-            "body" => Self::Body,
-            "p" => Self::Paragraph,
-            "r" => Self::Run,
-            "t" => Self::Text,
-            "pPr" => Self::Property,
-            "rPr" => Self::Property,
-            _ => Self::Unknown,
+    fn from_name(name: &OwnedName) -> Self {
+        let raw = format!(
+            "{}:{}",
+            name.prefix.as_ref().unwrap_or(&String::new()),
+            name.local_name
+        );
+        if name.prefix.is_none() || name.prefix.as_ref().unwrap().ne("w") {
+            return Self::Unknown(raw);
+        }
+        // 只匹配 w:x
+        match &*name.local_name {
+            "document" => Self::Document(raw),
+            "body" => Self::Body(raw),
+            "p" => Self::Paragraph(raw),
+            "r" => Self::Run(raw),
+            "t" => Self::Text(raw),
+            "pPr" => Self::ParagraphProperty(raw),
+            "rPr" => Self::RunProperty(raw),
+            "color" => Self::Color(raw),
+            _ => Self::Unknown(raw),
+        }
+    }
+    fn is_text(&self) -> bool {
+        match self {
+            Self::Text(_) => true,
+            _ => false,
+        }
+    }
+    fn is_run_property(&self) -> bool {
+        match self {
+            Self::RunProperty(_) => true,
+            _ => false,
+        }
+    }
+    fn is_color(&self) -> bool {
+        match self {
+            Self::Color(_) => true,
+            _ => false,
         }
     }
 }
@@ -184,15 +172,91 @@ struct Element {
     children: Vec<Rc<RefCell<Element>>>,
     attributes: HashMap<String, String>,
     literal_text: Option<String>, // 目前只有  w:t 有
+    depth: usize,                 // for debug
 }
 impl Element {
-    fn new(element_type: ElementType) -> Self {
+    /// new Element, 需要指定 parent 和 type, parent 可以为 None
+    fn new(
+        element_type: ElementType,
+        parent: &Option<Rc<RefCell<Element>>>,
+        attributes: Vec<OwnedAttribute>,
+        depth: usize,
+    ) -> Self {
+        let mut attrs = HashMap::new();
+        attributes.iter().for_each(|v| {
+            attrs.insert(v.name.local_name.clone(), v.value.clone());
+        });
         Self {
             element_type,
-            parent: None,
+            parent: parent.as_ref().map(Rc::downgrade),
             children: vec![],
-            attributes: HashMap::new(),
+            attributes: attrs,
             literal_text: None,
+            depth,
+        }
+    }
+    fn append_child(&mut self, child: Rc<RefCell<Element>>) {
+        self.children.push(child);
+    }
+
+    // 下面是一些辅助方法
+    /// 寻找本节点最近的 run property
+    fn find_run_property(element: &Option<Rc<RefCell<Element>>>) -> Option<Rc<RefCell<Element>>> {
+        if let Some(ele) = element {
+            if let Some(parent) = &ele.borrow().parent {
+                if let Some(parent) = parent.upgrade() {
+                    // find run property from parent's children
+                    for child in parent.borrow().children.iter() {
+                        if child.borrow().element_type.is_run_property() {
+                            return Some(Rc::clone(child));
+                        }
+                    }
+                    // if not found, goes up
+                    return Self::find_run_property(&Some(parent));
+                }
+            }
+        }
+        None
+    }
+
+    /// 如果自己是 run property, 从中获取 color 属性
+    fn get_color(element: &Option<Rc<RefCell<Element>>>) -> Option<String> {
+        if let Some(ele) = &element {
+            // 本身不是 run property
+            if ele.borrow().element_type.is_run_property() {
+                println!("+++ type not run pr");
+                return None;
+            }
+            // 从 children 中寻找 w:color
+            for child in ele.borrow().children.iter() {
+                let child_ref = child.borrow();
+                if child_ref.element_type.is_color() {
+                    return child_ref.attributes.get("val").map(|v| v.clone());
+                }
+            }
+            println!("+++ no color child");
+        }
+        println!("+++ none run pr");
+        None
+    }
+
+    fn display(root: &Option<Rc<RefCell<Element>>>) -> String {
+        if let Some(root_rc) = root {
+            let attrs: Vec<_> = root_rc
+                .borrow()
+                .attributes
+                .iter()
+                .map(|(k, v)| format!("{}={},", k, v))
+                .collect();
+            let indent = "  ".repeat(root_rc.borrow().depth);
+            format!(
+                "{}{:?}, attrs: {:?},",
+                indent,
+                root_rc.borrow().element_type,
+                attrs
+            )
+        } else {
+            "None<Element>".to_string()
         }
     }
 }
@@ -200,26 +264,96 @@ impl Element {
 struct Parsing {
     // 这里假设有一个唯一的 root
     root: Option<Rc<RefCell<Element>>>,
-    current: Option<Rc<RefCell<Element>>>,
+    cur: Option<Rc<RefCell<Element>>>,
+    depth: usize,
 }
 impl Parsing {
+    fn new() -> Self {
+        Self {
+            root: None,
+            cur: None,
+            depth: 0,
+        }
+    }
     fn feed_element(&mut self, name: OwnedName, attributes: Vec<OwnedAttribute>) {
-        if name.prefix.ne("w") {
-            println!("unknown prefix of name: {:?}", name);
-            return;
+        self.depth += 1;
+
+        let element_type = ElementType::from_name(&name);
+
+        // TODO remove me
+        if name.local_name.ne("r") {
+            let attrs: Vec<_> = attributes
+                .iter()
+                .map(|a| format!("{}={}", a.name.local_name, a.value))
+                .collect();
+            println!(
+                "{}{}, Type:{:?}, attributes: {:?}",
+                " ".repeat(self.depth),
+                name.local_name,
+                element_type,
+                attrs
+            );
         }
 
-        let element_type = ElementType::from_str(&name.local_name);
-        let element = Rc::new(RefCell::new(Element::new(element_type)));
-        if self.root.is_none() {
+        let element = Rc::new(RefCell::new(Element::new(
+            element_type,
+            &self.cur,
+            attributes,
+            self.depth,
+        )));
+        if let Some(cur_parent) = &self.cur {
+            // 最新节点添加为 parent 的子节点
+            cur_parent.borrow_mut().append_child(Rc::clone(&element));
+            // cur parent 变更为 最新节点
+            self.cur.replace(element);
+        } else {
             // 第一个节点
             self.root.replace(Rc::clone(&element));
-            self.current.replace(element);
-            return;
+            self.cur.replace(element);
         }
-        // 平级添加? 还是添加到 children 里
     }
-    fn fish_feed_element() {}
+    fn fish_feed_element(&mut self) {
+        self.depth -= 1;
+
+        // 当前父节点指向上一层的节点
+        let mut parent = None;
+        if let Some(cur) = &self.cur {
+            if let Some(p) = &cur.borrow().parent {
+                if let v = p.upgrade() {
+                    parent = v;
+                }
+            }
+        }
+
+        self.cur = parent;
+    }
     /// 目前只有 w:t 类型会有
-    fn feed_characters(&mut self, data: String) {}
+    fn feed_characters(&mut self, data: String) {
+        if let Some(cur) = &self.cur {
+            cur.borrow_mut().literal_text = Some(data);
+        }
+    }
+}
+
+fn print_elements(root: &Option<Rc<RefCell<Element>>>) {
+    println!("{}", Element::display(root));
+    if let Some(root_rc) = root {
+        if root_rc.borrow().element_type.is_text() {
+            let run_property = Element::find_run_property(&root);
+            let color_val = Element::get_color(&run_property);
+            let text = root_rc
+                .borrow()
+                .literal_text
+                .as_ref()
+                .map(|v| v.clone())
+                .unwrap_or_default();
+            println!("text: {}", text);
+            if let Some(run_pr) = run_property {
+                println!("----has run property---{:?}", color_val);
+            }
+        }
+        for child in root_rc.borrow().children.iter() {
+            print_elements(&Some(Rc::clone(child)));
+        }
+    }
 }
